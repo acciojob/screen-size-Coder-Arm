7a64b1dc40a266e0a80ba12dc459472a71b3f125
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-08 23:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 const div = document.getElementById("sizeInfo");
 const heading = div.firstElementChild;
-function display(){
+window.onload = function(){
 	heading.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 }
-window.onload = display();
-window.onresize = display();
+window.onresize = function(){
+	heading.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+} 
 
-  
+   
