5e59702196c727ff02c8ebab0860f80549f6d73d
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-08 23:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const div = document.getElementById("sizeInfo");
+const heading = div.firstElementChild;
+heading.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
